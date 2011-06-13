7d57e8e2bba09cdb91a4fbd80942944df12faa3b
Ealin@Mac <ealin.chiu@gmail.com>
2011-06-14 00:40:07 +0800

    Initial Commit

---
+++ HeyGoTimes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HeyGoTimes
+//
+//  Created by Apple on 2011/6/14.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
